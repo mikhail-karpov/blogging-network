c3f7cd486c9c070e4b30246610fc431b15dc0285
Mikhail Karpov <mikhailkarpov@hotmail.com>
2021-11-01 11:58:06 +0200

    Setup user-feed-service for development

---
+++ user-feed-service/src/test/java/com/mikhailkarpov/bloggingnetwork/feed/UserFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikhailkarpov.bloggingnetwork.feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
