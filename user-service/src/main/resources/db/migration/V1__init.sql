42a2a9e7961523b36b213b70ffc59add4644080f
Mikhail <mikhailkarpov@hotmail.com>
2021-11-18 12:08:46 +0200

    Migrating to PostgreSQL

---
+++ user-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE user_profile(
+    id VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_date TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE following(
+    follower_user_id VARCHAR(255) NOT NULL REFERENCES user_profile(id),
+    following_user_id VARCHAR(255) NOT NULL REFERENCES user_profile(id),
+    created_date TIMESTAMP NOT NULL,
+    PRIMARY KEY(follower_user_id, following_user_id)
+);
--- user-service/src/test/java/com/mikhailkarpov/users/config/AbstractIT.java
+++ user-service/src/test/java/com/mikhailkarpov/users/config/AbstractIT.java
@@ -4,19 +4,28 @@ import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 public abstract class AbstractIT {
+
+    static final PostgreSQLContainer POSTGRES;
 
     static final KeycloakContainer KEYCLOAK;
 
     static final GenericContainer REDIS;
 
     static {
+        POSTGRES = new PostgreSQLContainer("postgres")
+                .withDatabaseName("user_service")
+                .withUsername("user_service")
+                .withPassword("password");
+
         KEYCLOAK = new KeycloakContainer("jboss/keycloak:15.0.2")
                 .withRealmImportFile("./bloggingnetwork-realm.json");
 
         REDIS = new GenericContainer("redis").withExposedPorts(6379);
 
+        POSTGRES.start();
         KEYCLOAK.start();
         REDIS.start();
     }
@@ -33,5 +42,13 @@ public abstract class AbstractIT {
         registry.add("spring.redis.host", REDIS::getHost);
         registry.add("spring.redis.port", REDIS::getFirstMappedPort);
         registry.add("spring.cache.type", () -> "redis");
+    }
+
+    @DynamicPropertySource
+    static void configDatasource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> POSTGRES.getJdbcUrl());
+        registry.add("spring.datasource.driver-class-name", () -> POSTGRES.getDriverClassName());
+        registry.add("spring.datasource.username", () -> POSTGRES.getUsername());
+        registry.add("spring.datasource.password", () -> POSTGRES.getPassword());
     }
 }
